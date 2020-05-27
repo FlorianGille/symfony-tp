f6f467535d24ce8f5ddf84b06a83259d0d6546cd
Paul Godard <paul.godard@outlook.fr>
2020-05-27 16:07:10 +0200

    user fix

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,12 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles',ChoiceType::class,array(
+                'multiple'=>true,
+                'choices' => array('ADMIN' => 'ROLE_ADMIN',
+                    'PRODUIT' => 'ROLE_PRODUIT',
+                    'MARQUE'=> 'ROLE_MARQUE')
+            ))
             ->add('password')
         ;
     }
